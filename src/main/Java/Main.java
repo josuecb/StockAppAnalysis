b42e95fcfa455359fe6bed2ac76c0084a89656f7
Josue <anonjacmdev@hotmail.com>
2017-05-13 17:44:14 -0400

    Cleaning up test class

--- src/main/Java/Main.java
+++ src/main/Java/Main.java
@@ -1,7 +1,6 @@
 import bin.FileDownloader;
 import bin.yahoo.YahooLinkGen;
 
-import java.io.IOException;
 
 /**
  * Created by Josue on 5/13/2017.
@@ -13,21 +12,13 @@ public class Main {
         y.setIntervalDate(1, 1, 2016, true);
         // to interval
         y.setIntervalDate(1, 1, 2017, false);
-
+        FileDownloader d;
         try {
             System.out.println(y.getLink());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        FileDownloader d = null;
-        try {
+
             d = new FileDownloader(y.getLink());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
             d.start("C:\\Users\\Josue\\IdeaProjects\\StockAppAnalysis\\src\\main\\Java\\test");
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
